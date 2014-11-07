e863e9585b25d8762631c7b6ec076e9995290f40
Flyingmana <flyingmana@googlemail.com>
2014-11-07 02:18:55 +0100

    fill catalog_search_results from elastic search

--- app/code/community/Hackathon/ElasticgentoCatalogSearch/Model/Catalogsearch/Resource/Fulltext.php
+++ app/code/community/Hackathon/ElasticgentoCatalogSearch/Model/Catalogsearch/Resource/Fulltext.php
@@ -40,23 +40,133 @@ class Hackathon_ElasticgentoCatalogSearch_Model_Catalogsearch_Resource_Fulltext
      * override the method to prepare the result
      *
      * @param Mage_CatalogSearch_Model_Fulltext $object
-     * @param string $queryText
-     * @param Mage_CatalogSearch_Model_Query $query
+     * @param string                            $queryText
+     * @param Mage_CatalogSearch_Model_Query    $query
+     *
      * @return Hackathon_ElasticgentoCatalogSearch_Model_Catalogsearch_Resource_Fulltext|Mage_CatalogSearch_Model_Resource_Fulltext
      */
     public function prepareResult(
         $object,
         $queryText,
         $query
-    )
-    {
+    ) {
         $helper = Mage::helper('elasticgento_catalogsearch');
 
         if (!$helper->isSearchActive()) {
             return parent::prepareResult($object, $queryText, $query);
         }
 
-        $adapter = $helper->getAdapter();
+        if (!$query->getIsProcessed()) {
+            $adapter = $helper->getAdapter();
+            $result = $this->fetchSearchResultFromElasticSearch($adapter, $queryText, $query);
+            if ($result && $result instanceof \Elastica\ResultSet) {
+                $this->fillSearchResultInMagentoResultTable($result, $query);
+            }
+        }
+    }
+
+    /**
+     * @param Hackathon_ElasticgentoCore_Model_Resource_Client $searchAdapter
+     * @param string $queryText
+     * @param Mage_CatalogSearch_Model_Query $query
+     *
+     * @return Elastica\ResultSet
+     */
+    protected function fetchSearchResultFromElasticSearch(
+        $searchAdapter,
+        $queryText,
+        $query
+    )
+    {
+        $elasticQuery = new Elastica\Query();
+        $queryFuzzyLikeThis = new \Elastica\Query\FuzzyLikeThis();
+        $queryFuzzyLikeThis->addFields(
+            [
+                'name',
+                //'description',
+            ]
+        );
+        $queryFuzzyLikeThis->setLikeText($queryText);
+
+        $elasticQuery->setQuery($queryFuzzyLikeThis);
+
+        $returnFields = [
+            'entity_id',
+            'name',
+        ];
+
+        $elasticQuery->setFields($returnFields);
+        return $searchAdapter->getIndex($query->getStoreId())->search($elasticQuery);
+    }
+
+
+    /**
+     * @param \Elastica\ResultSet            $resultSet
+     * @param Mage_CatalogSearch_Model_Query $query
+     */
+    protected function fillSearchResultInMagentoResultTable(
+        \Elastica\ResultSet $resultSet,
+        Mage_CatalogSearch_Model_Query $query
+    )
+    {
+
+        $writeAdapter = $this->_getWriteAdapter();
+
+        $insertQuery = $this->buildInsertQuery($resultSet, $query->getId());
+
+        $writeAdapter->query($insertQuery);
+
+        $query->setIsProcessed(1);
 
     }
+
+    protected function buildInsertQuery( \Elastica\ResultSet $resultSet, $queryId )
+    {
+
+        $writeAdapter = $this->_getWriteAdapter();
+
+        $query = 'INSERT';
+
+        $query = sprintf('%s INTO %s', $query, $writeAdapter->quoteIdentifier($this->getTable('catalogsearch/result')));
+
+        $fields = [
+            'query_id',
+            'product_id',
+            'relevance',
+        ];
+
+        $columns   = array_map(array($writeAdapter, 'quoteIdentifier'), $fields);
+        $columns   = implode(',', $columns);
+
+        $query .= ' ('.$columns.')';
+
+        $query .= ' VALUES';
+
+        $valueRows = [];
+
+        foreach($resultSet->getResults() as $result){
+            $productId = $result->getData()['entity_id'][0];
+            $values = [
+                (int) $queryId,
+                (int) $productId,
+                (float) $result->getScore()
+            ];
+            $valueRows[] = '('.implode(', ',$values).')';
+        };
+
+        $query .= ' '.implode(', ', $valueRows);
+
+        $update = array();
+        foreach ($fields as $field) {
+            $update[] = sprintf('%1$s = VALUES(%1$s)', $writeAdapter->quoteIdentifier($field));
+        }
+
+        if ($update) {
+            $query = sprintf('%s ON DUPLICATE KEY UPDATE %s', $query, join(', ', $update));
+        }
+
+        return $query;
+
+    }
+
 }
